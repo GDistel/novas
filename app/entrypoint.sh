6f0180ee368b835cdccc2112c07a72f86832dcd4
German Distel <germandistel@gmail.com>
2020-06-14 11:15:49 -0300

    Production ready docker-compose

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "PostgreSQL started"
+    python manage.py flush --no-input
+fi
+
+python manage.py migrate
 python manage.py collectstatic --no-input --clear
+
 exec "$@"
--- app/novas/settings/production.py
+++ app/novas/settings/production.py
@@ -1,13 +1,27 @@
 from .base import *
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = [
+#     'localhost',
+#     '127.0.0.1',
+# ]
 
-CORS_ORIGIN_WHITELIST = ()
+#CORS_ORIGIN_WHITELIST = ()
 
-DEBUG = False
-SECURE_SSL_REDIRECT = True
-CSRF_COOKIE_SECURE = False
-SESSION_COOKIE_SECURE = True
-X_FRAME_OPTIONS = 'DENY'
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
+DEBUG = True
+# SECURE_SSL_REDIRECT = True
+# CSRF_COOKIE_SECURE = False
+# SESSION_COOKIE_SECURE = True
+# X_FRAME_OPTIONS = 'DENY'
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "novas",
+        "USER": "novas",
+        "PASSWORD": "novas",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
